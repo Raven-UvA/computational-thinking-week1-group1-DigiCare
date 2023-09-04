c33309d66fdf16bc2f8bad4dffa3afd66ef20f32
CalebAgoha <calebagoha@gmail.com>
2023-09-04 14:09:51 +0200

    file created to print team name and members

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,5 @@
+import Angelica, Angeline, caleb, Paula, file, RavenHaesakkers
+def our_names():
+    print("This is team DigiCare. We are:", Angelica.my_name(), Angeline.MyName(), caleb.my_name(), Paula.myname(), file.my_name(), RavenHaesakkers.MyName())
+
+our_names()
