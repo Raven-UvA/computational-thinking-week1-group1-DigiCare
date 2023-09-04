24aa8a57a8ede6699dbb51b1a6d4ea68e7af5ef0
CalebAgoha <calebagoha@gmail.com>
2023-09-04 16:55:05 +0200

    Act 2

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -3,12 +3,21 @@ def our_names():
     print("This is team DigiCare. We are:", Angelica.my_name(), Angeline.MyName(), caleb.my_name(), Paula.myname(), file.my_name(), RavenHaesakkers.MyName())
 
 def short_story():
+    #ACT 1
     print(Angelica.par1(), '\n') 
     print(Angeline.Act_1(), '\n') 
     print(caleb.par3(), '\n') 
     print(Paula.par4(), '\n')
     print(file.part5(), '\n')
     print(RavenHaesakkers.par6(), '\n')
+
+    #ACT 2
+    print(Angelica.par7(), "\n")
+    print(Angeline.Act_2(), "\n")
+    print(caleb.par9(), "\n")
+    print(Paula.par10(), "\n")
+    print(file.part11(), "\n")
+    print(RavenHaesakkers.par12(), "\n")
 
 our_names()
 short_story()
