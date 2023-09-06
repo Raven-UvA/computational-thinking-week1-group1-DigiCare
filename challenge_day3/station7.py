32c3df65df9a9fad503810fe41b89b6d7f346ff3
paulabiazik <paulabiazik23@gmail.com>
2023-09-06 12:10:03 +0200

    This is a message describing what I did

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -9,9 +9,5 @@ def solution_station_7(equation, a, b, c, d, e):
     result = eval(equation, {'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
     return result
     
-user_equation = input("Enter your equation:")
 
-result = solution_station_7(user_equation, a, b, c, d, e)
-
-print(f"Result: {result}")
     
