19edeb058e49f26b45ae6f9155ff3cf2619dc4ba
angelicasa <angelica.saglimbeni@student.uva.nl>
2023-09-06 11:32:21 +0200

    Function for station 2

---
+++ challenge_day3/station2.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+weekDays = ("月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日")
+
+def solution_station_2(date):
+    date_format = '%Y-%m-%d'
+    date_obj = datetime.strptime(date, date_format)
+    day = date_obj.weekday()
+    dayOfWeek = weekDays[day]
+    return dayOfWeek
+
+print(solution_station_2())
