1376c6da4b97303140d849a63770255ef146ca19
angelicasa <angelica.saglimbeni@student.uva.nl>
2023-09-06 12:44:30 +0200

    Finished dictionary

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -3,7 +3,13 @@ teams = {1: ["Isis", "Maria", "Mika", "Rosa", "Maj", "Damari"],
          3: ["Elisa", "Dmitry", "Martyna", "Jesper", "Kavi"],
          4: ["Kelt", "Sebastian", "Quanpu", "Ruben", "Sofia"],
          5: ["Sahir", "Tom", "Kian", "Teun", "Gonzalo"],
-         6: ["Angeline", "Matas", "Raven", "Caleb", "Paula", "Angelica"]
+         6: ["Angeline", "Matas", "Raven", "Caleb", "Paula", "Angelica"],
+         7: ["Mate", "Martyna", "Emma", "Vincent", "Eryk"], 
+         8: ["Sanne", "Beatris", "Sally", "Alyssa", "Caio", "Hielke"], 
+         9: ["Felix", "Atlas", "Yash", "Diana", "Eliana"], 
+         10:["Andrey", "Akanksha", "Max", "Hugo", "Al-fatihi"], 
+         11:["Ioanna", "Misha", "Vanesa", "Ella", "Alicja", "Cristian"],
+         12:["Alexia", "Karolina", "Jelle", "Luca", "Rachna", "Yuyue"]
          }
 
 
