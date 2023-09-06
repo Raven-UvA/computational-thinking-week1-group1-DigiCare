04539a2fede2d93a304550c4841f5d2e82204683
CalebAgoha <calebagoha@gmail.com>
2023-09-06 13:11:23 +0200

    final additions for stations

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -1,18 +1,6 @@
-teams = {1: ["Isis", "Maria", "Mika", "Rosa", "Maj", "Damari"], 
-         2: ["Maria", "Celia", "Gur", "Carine", "Emily", "Sabrina"], 
-         3: ["Elisa", "Dmitry", "Martyna", "Jesper", "Kavi"],
-         4: ["Kelt", "Sebastian", "Quanpu", "Ruben", "Sofia"],
-         5: ["Sahir", "Tom", "Kian", "Teun", "Gonzalo"],
-         6: ["Angeline", "Matas", "Raven", "Caleb", "Paula", "Angelica"],
-         7: ["Mate", "Martyna", "Emma", "Vincent", "Eryk"], 
-         8: ["Sanne", "Beatris", "Sally", "Alyssa", "Caio", "Hielke"], 
-         9: ["Felix", "Atlas", "Yash", "Diana", "Eliana"], 
-         10:["Andrey", "Akanksha", "Max", "Hugo", "Al-fatihi"], 
-         11:["Ioanna", "Misha", "Vanesa", "Ella", "Alicja", "Cristian"],
-         12:["Alexia", "Karolina", "Jelle", "Luca", "Rachna", "Yuyue"]
-         }
-
+import pandas as pd
+df = pd.read_csv("teams.csv")
 
 def solution_station_5(name):
-
+    return df.loc[df['members'] == name, 'team'].values[0]
 
