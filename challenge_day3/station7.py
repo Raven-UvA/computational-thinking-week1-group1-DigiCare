c879d8bd66c8ae3dfe2ddbbfce66f7ef40b4d148
paulabiazik <paulabiazik23@gmail.com>
2023-09-06 13:23:14 +0200

    This is a message describing what I did

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -4,7 +4,7 @@ c = 4
 d = 7
 e = 0.5
 
-def solution_station_7(equation, a, b, c, d, e):
+def solution_station_7(equation):
     
     result = eval(equation, {'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
     return result
